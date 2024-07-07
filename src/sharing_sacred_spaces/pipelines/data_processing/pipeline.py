155254f559abd1e9dbd3d9ae777a2238d5539487
Jed Gore <jedgore@Jeds-MacBook-Pro.local>
2024-07-07 11:12:18 -0400

    set up initial run

--- src/sharing_sacred_spaces/pipelines/data_processing/pipeline.py
+++ src/sharing_sacred_spaces/pipelines/data_processing/pipeline.py
@@ -1,28 +1,16 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
+from .nodes import preprocess_fbi_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
-            ),
-            node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
-            ),
-            node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
+                func=preprocess_fbi_data,
+                inputs="fbi_data",
+                outputs="preprocessed_fbi_data",
+                name="preprocess_fbi_data_node",
             ),
         ]
     )
